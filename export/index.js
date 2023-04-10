cd96fb77eeb90e02928e7cf2b008027d0b55247d
Ayush Jain <116271677+AyushJainA@users.noreply.github.com>
2023-04-10 12:42:31 +0530

    Create index.js

---
+++ export/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+// const xyz = require("./oper");
+
+const {add,sub,m}= require("./oper");
+
+// console.log(xyz);
+
+// console.log(xyz.add(5,6));
+// console.log(xyz.sub(55,555));
+// console.log(xyz.m(5,9,0));
+
+console.log(add(5,6));
