7343c51edc55909868a24c934f8139c5bef6ebc0
Ayush Jain <116271677+AyushJainA@users.noreply.github.com>
2023-04-10 12:42:59 +0530

    Create oper.js

---
+++ export/oper.js
@@ -0,0 +1,20 @@
+//console.log("operator");
+
+const add = (a , b) =>{
+     return a+b;
+} 
+
+const sub = (a,b)=>{
+    return a - b;
+}
+
+const m = ( a, b, c) =>{
+    return (a*b)*c;
+}
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.m = m;
+
+
+module.exports ={add,sub,m};
